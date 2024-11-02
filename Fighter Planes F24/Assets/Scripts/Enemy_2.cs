c36e1db0b2345671399cd3c140b2096f60dbaf31
Norwill <willeverett206@gmail.com>
2024-11-01 22:48:28 -0400

    Enemy_2 spawn and movement
    Changed how the enemy_2 spawns and moves

--- Fighter Planes F24/Assets/Scripts/Enemy_2.cs
+++ Fighter Planes F24/Assets/Scripts/Enemy_2.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Enemy_2 : MonoBehaviour
@@ -7,12 +8,17 @@ public class Enemy_2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+      transform.Rotate(0, 0, 25f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(new Vector3(0, -3f, 0) * Time.deltaTime * 2f);
+
+        if (transform.position.y < -8f)
+        {
+            Destroy(this.gameObject);
+        }
     }
 }
--- Fighter Planes F24/Assets/Scripts/GameManager.cs
+++ Fighter Planes F24/Assets/Scripts/GameManager.cs
@@ -7,12 +7,14 @@ public class GameManager : MonoBehaviour
 
     public GameObject player;
     public GameObject enemy;
+    public GameObject enemy_2;
 
     // Start is called before the first frame update
     void Start()
     {
         Instantiate(player, transform.position, Quaternion.identity);
         InvokeRepeating("CreateEnemy", 1f, 3f);
+        InvokeRepeating("CreateEnemy_2", 5f, 6f);
     }
 
     // Update is called once per frame
@@ -25,4 +27,8 @@ public class GameManager : MonoBehaviour
     {
         Instantiate(enemy, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
     }
-}
+    void CreateEnemy_2()
+    {
+        Instantiate(enemy_2, new Vector3(Random.Range(-9f, 9f), 8f, 0), Quaternion.identity);
+    }
+}   
